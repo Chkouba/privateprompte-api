1ccafd8d9be9709a139353f43128840e0b18f663
Chkouba <allal.chakib@gmail.com>
2025-03-15 12:03:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
 from flask import Flask, request, jsonify
 from presidio_analyzer import AnalyzerEngine
 from presidio_anonymizer import AnonymizerEngine
+import os  # ajout important !
 
 app = Flask(__name__)
 
-# Initialiser les moteurs de Presidio
 analyzer = AnalyzerEngine()
 anonymizer = AnonymizerEngine()
 
 @app.route('/anonymize', methods=['POST'])
-def anonymize_prompt():
+def anonymize():
     data = request.get_json()
     prompt = data.get('prompt', '')
 
-    # Analyser le texte (identification des infos sensibles)
     analyzer_results = analyzer.analyze(text=prompt, language='fr')
-
-    # Anonymiser le texte
     anonymized_text = anonymizer.anonymize(text=prompt, analyzer_results=analyzer_results)
 
     return jsonify({"anonymized_text": anonymized_text.text})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
